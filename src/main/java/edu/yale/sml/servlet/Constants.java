66ebb261d1186fa6e5ff5ccbf589640e1bd2e142
osmandin <osmandin@gmail.com>
2015-05-26 13:39:00 -0400

    Update shelving ref

--- src/main/java/edu/yale/sml/servlet/Constants.java
+++ src/main/java/edu/yale/sml/servlet/Constants.java
@@ -12,7 +12,7 @@ public interface Constants {
     /** business logic dependent */
     final String ADMIN_CODE = "Admin";
 
-    final String PERMISSIOSN_PAGE = "/shelfscan/pages/permissions.xhtml";
+    final String PERMISSIOSN_PAGE = "/shelving/pages/permissions.xhtml";
 
     final String CAS_VALIDATE_URL = "https://secure.its.yale.edu/cas/validate";
 
